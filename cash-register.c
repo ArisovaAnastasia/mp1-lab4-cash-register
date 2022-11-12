ad041ae05251e4659a410c27b4fa89eb9207413f
uf0logic <ufologic@protonmail.com>
2022-11-13 00:05:32 +0300

    Now first and third modes are in loop, which means you don't need to specify mode each time you want to add a new item either to database or to your shopping cart

--- cash-register.c
+++ cash-register.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdbool.h>
 
 // Creating structure for items
 struct item {
@@ -17,15 +16,19 @@ void print_item(int item_id, struct item db[]) {
     printf(" Name: %s; Price: %d; Discount: %d \n", db[item_id].name, db[item_id].price, db[item_id].discount);
 }
 
-void enter_item_data(int item_id, struct item db[]) {
+int enter_item_data(int item_id, struct item db[]) {
     int id_temp;
     printf("Enter new item's data (id, name, price, discount): ");
-    scanf("%d%s%d%d", &id_temp, db[item_id].name, &db[item_id].price, &db[item_id].discount);
+    scanf("%d", &id_temp);
+    if (id_temp != 0) {
+        scanf("%s%d%d", db[item_id].name, &db[item_id].price, &db[item_id].discount);
 
-    for (int j = 3; j >= 0; j--) {
-        db[item_id].id[j] = id_temp % 10;
-        id_temp /= 10;
+        for (int j = 3; j >= 0; j--) {
+            db[item_id].id[j] = id_temp % 10;
+            id_temp /= 10;
+        }
     }
+    else return 0;
 }
 
 int main() {
@@ -33,7 +36,6 @@ int main() {
 
     struct item db[10];   // For now database is limited
     struct item cart[50]; // Shopping cart (also limited - can use vectors later, to make infinite capacity)
-    bool flag  = true;
 
     for (int i = 0; i < 10; i++) db[i].quantity = 0;
 
@@ -41,7 +43,7 @@ int main() {
            "3)Enter your items \n 4)Generate receipt \n\n 0)Exit the program \n\n");
 
     // Infinite while-loop to run the program constantly
-    while (true){
+    while (1){
         do{
             printf("Mode: ");
             scanf("%d",&mode);
@@ -51,8 +53,11 @@ int main() {
         switch(mode){
             case 0: return 0;
             case 1:
-                enter_item_data(new_item_id, db);
-                new_item_id++;
+                printf("Type 0 to quit\n\n");
+                while (1) {
+                    if (enter_item_data(new_item_id, db) != 0) new_item_id++;
+                    else break;
+                }
                 break;
             case 2:
                 printf("Enter item's id: ");
@@ -70,16 +75,20 @@ int main() {
                 }
                 break;
             case 3:
-                printf("Add item to the receipt (Enter item's id): ");
-                scanf("%d", &item_id);
-                for (int i = 0; i <= 10; i++) {
-                    if (i == 10) {
-                        printf("Item not found\n\n");
-                        break;
-                    }
-                    if (item_id == db[i].id[0] * 1000 + db[i].id[1] * 100 + db[i].id[2] * 10 + db[i].id[3]) {
-                        db[i].quantity++;
-                        break;
+                printf("Type 0 to quit\n\n");
+                while(1) {
+                    printf("Add item to the receipt (Enter item's id): ");
+                    scanf("%d", &item_id);
+                    if (item_id == 0) break;
+                    for (int i = 0; i <= 10; i++) {
+                        if (i == 10) {
+                            printf("Item not found\n\n");
+                            break;
+                        }
+                        if (item_id == db[i].id[0] * 1000 + db[i].id[1] * 100 + db[i].id[2] * 10 + db[i].id[3]) {
+                            db[i].quantity++;
+                            break;
+                        }
                     }
                 }
                 break;
