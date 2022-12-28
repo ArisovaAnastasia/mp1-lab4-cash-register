b2cba1505fee56b1530fe98e33e8cb002d945eac
uf0logic <ufologic@protonmail.com>
2022-12-28 19:11:48 +0300

    fixed solution. Final

--- cash-register.c
+++ cash-register.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <string.h>
 
 // Creating structure for items
 struct item {
-	int id[4];
+	char id[5];
     char name[32];
 	int price;
 	int discount;
@@ -11,31 +12,22 @@ struct item {
 
 // Implementing some basic functions to make main function more readable
 void print_item(int item_id, struct item db[]) {
-    printf("ID: ");
-    for (int j = 0; j < 4; j ++) printf("%d", db[item_id].id[j]);
-    printf(" Name: %s; Price: %d; Discount: %d \n", db[item_id].name, db[item_id].price, db[item_id].discount);
+    printf("ID: %s Name: %s; Price: %d; Discount: %d \n", db[item_id].id, db[item_id].name, db[item_id].price, db[item_id].discount);
 }
 
 int enter_item_data(int item_id, struct item db[]) {
-    int id_temp;
     printf("Enter new item's data (id, name, price, discount): ");
-    scanf("%d", &id_temp);
-    if (id_temp != 0) {
-        scanf("%s%d%d", db[item_id].name, &db[item_id].price, &db[item_id].discount);
-
-        for (int j = 3; j >= 0; j--) {
-            db[item_id].id[j] = id_temp % 10;
-            id_temp /= 10;
-        }
-    }
-    else return 0;
+    scanf("%s", db[item_id].id);
+    if (strcmp(db[item_id].id, "0") == 0) return 0;
+    scanf("%s%d%d", db[item_id].name, &db[item_id].price, &db[item_id].discount);
+    return 1;
 }
 
 int main() {
-	int mode, new_item_id = 0, item_id = 0, sum = 0;
+	int mode, sum, new_item_id = 0;
+    char item_id[5];
 
     struct item db[10];   // For now database is limited
-    struct item cart[50]; // Shopping cart (also limited - can use vectors later, to make infinite capacity)
 
     for (int i = 0; i < 10; i++) db[i].quantity = 0;
 
@@ -61,14 +53,14 @@ int main() {
                 break;
             case 2:
                 printf("Enter item's id: ");
-                scanf("%d", &item_id);
+                scanf("%s",item_id);
                 //scanner(item_id, db);
                 for (int i = 0; i <= 10; i++) {
                     if (i == 10) {
                         printf("Item not found");
                         break;
                     }
-                    if (item_id == db[i].id[0] * 1000 + db[i].id[1] * 100 + db[i].id[2] * 10 + db[i].id[3]) {
+                    if (strcmp(item_id, db[i].id) == 0) {
                         print_item(i,db);
                         break;
                     }
@@ -78,14 +70,14 @@ int main() {
                 printf("Type 0 to quit\n\n");
                 while(1) {
                     printf("Add item to the receipt (Enter item's id): ");
-                    scanf("%d", &item_id);
-                    if (item_id == 0) break;
+                    scanf("%s",item_id);
+                    if (strcmp(item_id, "0") == 0) break;
                     for (int i = 0; i <= 10; i++) {
                         if (i == 10) {
                             printf("Item not found\n\n");
                             break;
                         }
-                        if (item_id == db[i].id[0] * 1000 + db[i].id[1] * 100 + db[i].id[2] * 10 + db[i].id[3]) {
+                        if (strcmp(item_id, db[i].id) == 0) {
                             db[i].quantity++;
                             break;
                         }
@@ -93,6 +85,7 @@ int main() {
                 }
                 break;
             case 4:
+                sum = 0;
                 printf("Generating receipt............\n");
                 for (int i = 0; i < 10; i++) {
                     if (db[i].quantity != 0) {
@@ -108,6 +101,4 @@ int main() {
                 break;
         }
     }
-
-	return 0;
 }
